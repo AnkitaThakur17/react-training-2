775ce2c4cabed0dee0a4df54110fc5909eb6bb53
Ankita Thakur <ankita.mindiii@gmail.com>
2025-09-05 19:19:22 +0530

    pushed code to old repo

---
+++ objectMethods.js
@@ -0,0 +1,11 @@
+//Object.keys method
+const user = {name : "ankita", age:22, city:"Indore"}
+console.log(Object.keys(user)) // o/p  ['name', 'age', 'city']
+
+//Object.values 
+console.log(Object.values(user)) // o/p ['ankita', 22, 'Indore']
+
+//Object.entries
+console.log(Object.entries(user)) // o/p [["name","Ankita"], ["age",22], ["city","Delhi"]]
+
+
